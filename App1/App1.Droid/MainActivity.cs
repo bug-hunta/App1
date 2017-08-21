e9bf417375b04b5304dacbde9126cad8b3c12128
Summy <mtnevento@gmail.com>
2017-08-21 19:20:43 +0000

    Add project files.

---
+++ App1/App1.Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1.Droid
+{
+	[Activity (Label = "FasLoad", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 234;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.mPress);
+
+           
+
+            button.Click += delegate {
+				button.Text = string.Format ("Crust Media is now live on xamarin with {0} click(s)!", --count);
+			};
+		}
+	}
+}
+
+
